19f9e9ec9bc02d3268878e5bcae729cc139a328d
enjinerd <dev@ron.my.id>
2022-09-18 13:37:07 +0700

    chore(app): init template

---
+++ vite.config.ts
@@ -0,0 +1,46 @@
+import react from '@vitejs/plugin-react';
+import { readdirSync } from 'fs';
+import path from 'path';
+import { defineConfig } from 'vite';
+import ViteFonts from 'vite-plugin-fonts';
+import { VitePWA } from 'vite-plugin-pwa';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+// https://stackoverflow.com/questions/69424422/use-compileroptions-baseurl-with-vite-js-project
+const absolutePathAliases = {};
+// Root resources folder
+const srcPath = path.resolve('./src/');
+// Ajust the regex here to include .vue, .js, .jsx, etc.. files from the resources/ folder
+const srcRootContent = readdirSync(srcPath, {
+  withFileTypes: true,
+}).map((dirent) => dirent.name.replace(/(\.ts){1}(x?)/, ''));
+
+srcRootContent.forEach((directory) => {
+  absolutePathAliases[directory] = path.join(srcPath, directory);
+});
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      ...absolutePathAliases,
+    },
+  },
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    ViteFonts({
+      google: {
+        families: ['Plus Jakarta Sans', 'Inter'],
+      },
+    }),
+    VitePWA({
+      devOptions: {
+        enabled: true,
+        /* other options */
+      },
+    }),
+  ],
+  build: {
+    chunkSizeWarningLimit: 2000,
+  },
+});
