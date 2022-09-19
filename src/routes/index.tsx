9b41037c3ae944d519b938d6ccd423499eea2cd1
enjinerd <dev@ron.my.id>
2022-09-19 13:56:43 +0700

    feat(dashboard): page layouting

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home, NotFound } from "pages/public";
 import { Login, Register } from "pages/public/auth";
 import Private from "./Private";
+import { Dashboard } from "pages/private/Dashboard";
 
 export default function Router() {
   return (
@@ -10,6 +11,7 @@ export default function Router() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/app" element={<Dashboard />} />
         <Route path="/*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
